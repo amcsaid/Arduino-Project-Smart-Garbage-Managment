02e33d7d2f1b228cf94900bc1ff01774cdf6cdd8
Said A <said.amchart@gmail.com>
2021-02-27 21:32:07 +0100

    Update AR2_GarbageSensors.ino

--- AR2_GarbageSensors.ino
+++ AR2_GarbageSensors.ino
@@ -1,49 +1,95 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
-#define trigPin 6
-#define echoPin 7
-SoftwareSerial myser(2,3);// RX,TX
-Servo servo;
-int sound = 250;
+#define trigPin 6  // ultrasonic triger motor
+#define echoPin 7  // ultrasonic echo  motor
+Servo servo; // motor
+
+
+
+
+SoftwareSerial myser(2,3);// TX,RX
+#define trigPint 4  // ultrasonic 2 bt
+#define echoPint 5  // ultrasonic 2 bt
 
 
 void setup() {
-    myser.begin(9600);
-    Serial.begin (9600);
-    pinMode(trigPin, OUTPUT);
-    pinMode(echoPin, INPUT);
-    servo.attach(10);
+  // ultrasonic 1 avec motor
+  pinMode(trigPin, OUTPUT); // ultrasonic triger motor
+  pinMode(echoPin, INPUT); // ultrasonic triger motor
+  servo.attach(10); //  motor
+
+  Serial.begin(9600);
+  // ultrasonic 2 avec BT
+  pinMode(trigPint, OUTPUT);
+  pinMode(echoPint, INPUT);
+  myser.begin(9600);
 }
 
+
 void loop() {
-    long duration, distance;
+// ultrasonic 1 avec motor
+  long duration, distance;
 
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    duration = pulseIn(echoPin, HIGH);
-    distance = (duration/2) / 29.1;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
 
-    if (distance < 10) {
-        Serial.println("the distance is less than 10");
-        servo.write(90);
-        delay(1500);
-    } else {
-        servo.write(0);
-    }
+  duration = pulseIn(echoPin, HIGH);
+
+  if (duration < 680) { // eq to 10cm
+    servo.write(90);
+    delay(1500);
+  } 
+  else {
+    servo.write(0);
+  }
+
+  //
+  //
+  // UltraSonic 2 avec BT
+  //
+  // 
+  long durationt;
+  int toprint;
+
+  digitalWrite(trigPint, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPint, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPint, LOW);
+
+  durationt = pulseIn(echoPint, HIGH);
+  Serial.println(durationt);
   
-    if (distance > 60 || distance <= 0){
-        Serial.println("The distance is more than 60");
-    }
-    else {
-      Serial.print(distance);
-      Serial.println(" cm");
-    }
+
+  if (durationt > 1800) { // eq to 10cm
+    
+  } else if (durationt > 1600) {
+    toprint = 1;
+  } else if (durationt > 1440) {
+    toprint = 2;
+  } else if (durationt > 1280) {
+    toprint = 3;
+    }else if (durationt > 1120) {
+    toprint = 4;
+  }else if (durationt > 960) {
+    toprint = 5;
+   }else if (durationt > 800) {
+    toprint = 6;
+  }else if (durationt > 640) {
+    toprint = 7;
+  } else if (durationt > 480) {
+    toprint = 8;
+  } else {
+    toprint = 9;
+  } 
   
-    delay(500);
-    if(Serial.available()>0){
-        myser.write(Serial.read());
-    }
+  myser.write(toprint+48);
+  
+
+  delay(500);
+  
+  
 }
