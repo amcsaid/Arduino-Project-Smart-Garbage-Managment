c3acf3f3d3e969369e93ec65609a6664fe94454c
Said A <said.amchart@gmail.com>
2021-02-27 22:26:35 +0100

    Update AR2_GarbageSensors.ino

--- AR2_GarbageSensors.ino
+++ AR2_GarbageSensors.ino
@@ -1,33 +1,41 @@
 #include <Servo.h>
 #include <SoftwareSerial.h>
-#define trigPin 6  // ultrasonic triger motor
-#define echoPin 7  // ultrasonic echo  motor
-Servo servo; // motor
 
-
-
-
-SoftwareSerial myser(2,3);// TX,RX
+// US 1: 
 #define trigPint 4  // ultrasonic 2 bt
 #define echoPint 5  // ultrasonic 2 bt
 
+// US 2: 
+#define trigPin 6  // ultrasonic triger motor
+#define echoPin 7  // ultrasonic echo  motor
+
+// BT Master
+SoftwareSerial myser(2,3);// TX,RX
+
+// Moteur
+Servo servo; 
 
 void setup() {
   // ultrasonic 1 avec motor
   pinMode(trigPin, OUTPUT); // ultrasonic triger motor
   pinMode(echoPin, INPUT); // ultrasonic triger motor
-  servo.attach(10); //  motor
-
-  Serial.begin(9600);
+  
   // ultrasonic 2 avec BT
   pinMode(trigPint, OUTPUT);
   pinMode(echoPint, INPUT);
   myser.begin(9600);
+  
+  //  motor
+  servo.attach(10); 
 }
 
 
 void loop() {
-// ultrasonic 1 avec motor
+  //
+  //
+  // UltraSonic 2 avec BT
+  //
+  // 
   long duration, distance;
 
   digitalWrite(trigPin, LOW);
@@ -48,7 +56,7 @@ void loop() {
 
   //
   //
-  // UltraSonic 2 avec BT
+  // UltraSonic 1 avec BT
   //
   // 
   long durationt;
@@ -64,32 +72,28 @@ void loop() {
   Serial.println(durationt);
   
 
-  if (durationt > 1800) { // eq to 10cm
+  if (durationt > 1800) { // eq a vide
     
-  } else if (durationt > 1600) {
+  } else if (durationt > 1600)  {
     toprint = 1;
-  } else if (durationt > 1440) {
+  } else if (durationt > 1440)  {
     toprint = 2;
-  } else if (durationt > 1280) {
+  } else if (durationt > 1280)  {
     toprint = 3;
-    }else if (durationt > 1120) {
+  } else if (durationt > 1120)  {
     toprint = 4;
-  }else if (durationt > 960) {
+  } else if (durationt > 960)   {
     toprint = 5;
-   }else if (durationt > 800) {
+  } else if (durationt > 800)   {
     toprint = 6;
-  }else if (durationt > 640) {
+  } else if (durationt > 640)   {
     toprint = 7;
-  } else if (durationt > 480) {
+  } else if (durationt > 480)   {
     toprint = 8;
   } else {
     toprint = 9;
   } 
   
   myser.write(toprint+48);
-  
-
   delay(500);
-  
-  
 }
